934635eacecb7a23257392b231ae9b6096f8c4be
SandersWangBin <sanderswangbin@gmail.com>
2017-07-27 14:39:45 +0200

    easyCurl: update the TS_OM_BASIC

--- testsuites/TS_OM_BASIC.py
+++ testsuites/TS_OM_BASIC.py
@@ -13,17 +13,20 @@ def generateTs(args):
     print fileNames
     e = getErcConfig(getArgv(fileNames))
 
-    return {'TS_PM_BASIC': \
+    return {'TS_OM_BASIC': \
                {KEYWORD_TC: \
                    [ \
-                   {'TC_BASIC_BULK': {KEYWORD_RC:[\
-                        e.rc('RC_GROUPS_GET_WRONGURL'), \
-                        e.rc('RC_GROUPS_GET_WRONGBODY'), \
+                   {'TC_BASIC_OM': {KEYWORD_RC:[\
                         e.rc('RC_GROUPS_GET'), \
                         e.rc('RC_GROUPS_POST'), \
                         e.rc('RC_GROUPS_GET'), \
                         e.rc('RC_GROUPS_DELETE'), \
                         e.rc('RC_GROUPS_GET'), \
+                   ]}},\
+                   {'TC_GET': {KEYWORD_RC:[\
+                        e.rc('RC_GROUPS_GET'), \
+                        e.rc('RC_GROUPS_GET_WRONGURL'), \
+                        e.rc('RC_GROUPS_GET_WRONGBODY'), \
                    ]}},\
                    ]
                }
